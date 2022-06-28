d5e31ede290f5cece907580e4446bab228749050
Oliver Vicent <101949580+OliverV-WD@users.noreply.github.com>
2022-06-27 23:02:38 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const menuTrigger = document.querySelector('.menu-trigger'),
+    closeTrigger = document.querySelector('.close'),
+    giveClass = document.querySelector('.site');
+
+menuTrigger.addEventListener('click', function(){
+        giveClass.classList.toggle('showmenu')
+    })
+closeTrigger.addEventListener('click', function(){
+        giveClass.classList.remove('showmenu')
+    })
+
+
+// animate items on scroll   
+const io = new IntersectionObserver(entries => {
+    entries.forEach(entry =>{
+        if(entry.intersectionRatio > 0) {
+            entry.target.classList.add('this')
+        }
+    })
+})
+const boxEllist = document.querySelectorAll('.animate');
+boxEllist.forEach((el)=> {
+    io.observe(el)
+})
